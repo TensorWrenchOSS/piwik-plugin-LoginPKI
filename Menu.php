cfe5ca7919d66979db4cb89b2767f4fed5317f2c
Greg Ziskind <gregory.ziskind@synergistcomputing.com>
2015-03-03 11:29:38 -0500

    Added menu override to remove signout ability

---
+++ Menu.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\LoginPKI;
+
+use Piwik\Piwik;
+
+class Menu extends \Piwik\Plugin\Menu
+{
+    public function configureTopMenu(MenuTop $menu)
+    {
+    	$menu->remove('General_Logout');
+    }
+}
